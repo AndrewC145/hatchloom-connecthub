0ea7d4f4e6cba9ba9fda4c7bff1eb6f82176ab30
Andrew Cao <andrewcao618@gmail.com>
2026-03-17 18:56:39 -0400

    Added cursor JPA repository

--- src/main/java/com/hatchloom/connecthub/connecthub_service/repository/ClassifiedPostRepository.java
+++ src/main/java/com/hatchloom/connecthub/connecthub_service/repository/ClassifiedPostRepository.java
@@ -1,6 +1,10 @@
 package com.hatchloom.connecthub.connecthub_service.repository;
 import com.hatchloom.connecthub.connecthub_service.model.ClassifiedPost;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,4 +13,11 @@ public interface ClassifiedPostRepository extends JpaRepository<ClassifiedPost, 
     Optional<ClassifiedPost> getClassifiedPostById(Integer id);
 
     List<ClassifiedPost> findByStatus(String status);
+
+    List<ClassifiedPost> findByStatusOrderByCreatedAtDescIdDesc(String status, Pageable pageable);
+
+    @Query("""
+    SELECT c FROM ClassifiedPost c WHERE c.status = :status AND (c.createdAt < :cursorCreatedAt OR (c.createdAt = :cursorCreatedAt AND c.id < :cursorId)) ORDER BY c.createdAt DESC, c.id DESC
+""")
+    List<ClassifiedPost> findByStatusWithCursor(String status, LocalDateTime cursorCreatedAt, Integer cursorId, Pageable pageable);
 }
