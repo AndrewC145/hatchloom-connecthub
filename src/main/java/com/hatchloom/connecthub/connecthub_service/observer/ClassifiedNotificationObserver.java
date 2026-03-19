b3d67f308fd2b8eea064e24b0732a995267fa77d
Andrew Cao <andrewcao618@gmail.com>
2026-03-18 22:17:08 -0400

    Added notification service and classified notification observer

--- src/main/java/com/hatchloom/connecthub/connecthub_service/observer/ClassifiedNotificationObserver.java
+++ src/main/java/com/hatchloom/connecthub/connecthub_service/observer/ClassifiedNotificationObserver.java
@@ -1,7 +1,28 @@
 package com.hatchloom.connecthub.connecthub_service.observer;
 
+import com.hatchloom.connecthub.connecthub_service.builder.NotificationBuilder;
+import com.hatchloom.connecthub.connecthub_service.enums.NotificationType;
+import com.hatchloom.connecthub.connecthub_service.model.ClassifiedPost;
+import com.hatchloom.connecthub.connecthub_service.service.NotificationService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ClassifiedNotificationObserver implements ClassifiedObserver {
+    private final NotificationService notificationService;
+
+    public ClassifiedNotificationObserver(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+
+    @Override
+    public void update(ClassifiedPost post, Integer receiverUserId) {
+        notificationService.createNotification(
+                new NotificationBuilder()
+                .setRecipientUserId(receiverUserId)
+                .setSenderUserId(post.getAuthor())
+                .setType(NotificationType.CLASSIFIED_CREATED)
+                .setMessage("New classified post: " + post.getTitle().substring(0, 20))
+                .setClassifiedPostId(post.getId()));
+    }
 }
--- src/main/java/com/hatchloom/connecthub/connecthub_service/service/NotificationService.java
+++ src/main/java/com/hatchloom/connecthub/connecthub_service/service/NotificationService.java
@@ -1,4 +1,49 @@
 package com.hatchloom.connecthub.connecthub_service.service;
 
+import com.hatchloom.connecthub.connecthub_service.builder.NotificationBuilder;
+import com.hatchloom.connecthub.connecthub_service.model.Notification;
+import com.hatchloom.connecthub.connecthub_service.repository.NotificationRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    public void createNotification(NotificationBuilder builder) {
+        Notification notification = builder.build();
+        notificationRepository.save(notification);
+    }
+
+    public List<Notification> getNotifications(Integer userId, boolean unread) {
+        if (unread) {
+            return notificationRepository.findByRecipientUserIdAndIsReadFalseOrderByCreatedAtDesc(userId);
+        }
+        else {
+            return notificationRepository.findByRecipientUserIdOrderByCreatedAtDesc(userId);
+        }
+    }
+
+    public void markAsRead(Integer notificationId, Integer userId) {
+        Optional<Notification> notification = Optional.of(notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new IllegalArgumentException("Notification does not exist")));
+
+        Notification n = notification.get();
+        if (!n.getRecipientUserId().equals(userId)) {
+            throw new IllegalArgumentException("Notification does not belong to this user");
+        }
+
+        n.setRead(true);
+        n.setReadAt(LocalDateTime.now());
+        notificationRepository.save(n);
+    }
+
+
 }
