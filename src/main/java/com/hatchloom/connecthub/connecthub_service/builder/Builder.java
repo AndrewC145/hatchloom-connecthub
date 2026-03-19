8f408b200440e07b9b580894c79eb4dbe3af5ee5
Andrew Cao <andrewcao618@gmail.com>
2026-03-18 21:50:32 -0400

    Added builder

---
+++ src/main/java/com/hatchloom/connecthub/connecthub_service/builder/Builder.java
@@ -0,0 +1,15 @@
+package com.hatchloom.connecthub.connecthub_service.builder;
+
+import com.hatchloom.connecthub.connecthub_service.enums.NotificationType;
+import com.hatchloom.connecthub.connecthub_service.model.Notification;
+
+public interface Builder {
+    Builder setRecipientUserId(Integer recipientId);
+    Builder setSenderUserId(Integer senderId);
+    Builder setType(NotificationType type);
+    Builder setMessage(String message);
+    Builder setClassifiedPostId(Integer classifiedPostId);
+    Builder setConversationId(Integer conversationId);
+    Builder setMessageId(Integer messageId);
+    Notification build();
+}
