5cecb36afb5781692168c712fc3a56e3b11da7e2
Andrew Cao <andrewcao618@gmail.com>
2026-03-18 20:55:33 -0400

    Deleted old observer

--- src/main/java/com/hatchloom/connecthub/connecthub_service/observer/FeedSubject.java
+++
@@ -1,9 +0,0 @@
-package com.hatchloom.connecthub.connecthub_service.observer;
-
-import com.hatchloom.connecthub.connecthub_service.model.Post;
-
-public interface FeedSubject {
-    void attach(Observer observer);
-    void detach(Observer observer);
-    void notifyObservers(Post post);
-}
--- src/main/java/com/hatchloom/connecthub/connecthub_service/observer/Observer.java
+++
@@ -1,7 +0,0 @@
-package com.hatchloom.connecthub.connecthub_service.observer;
-
-import com.hatchloom.connecthub.connecthub_service.model.Post;
-
-public interface Observer {
-    void update(Post post);
-}
