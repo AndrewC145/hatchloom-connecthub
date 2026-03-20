e7a6b64b29e5a9deb9e5d87aa607878219458b88
Andrew Cao <andrewcao618@gmail.com>
2026-03-19 21:36:45 -0400

    Removed unused methods

--- src/main/java/com/hatchloom/connecthub/connecthub_service/repository/NotificationRepository.java
+++ src/main/java/com/hatchloom/connecthub/connecthub_service/repository/NotificationRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-    List<Notification> findByRecipientUserIdOrderByCreatedAtDesc(Integer recipientUserId);
-    List<Notification> findByRecipientUserIdAndIsReadFalseOrderByCreatedAtDesc(Integer recipientUserId);
-
     List<Notification> findByRecipientUserIdAndTypeOrderByCreatedAtDesc(Integer recipientUserId, NotificationType type);
     List<Notification> findByRecipientUserIdAndTypeAndIsReadFalseOrderByCreatedAtDesc(Integer recipientUserId, NotificationType type);
 }
