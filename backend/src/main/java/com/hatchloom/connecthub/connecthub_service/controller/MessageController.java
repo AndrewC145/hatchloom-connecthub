b5b5dfec39c9a89e8c0712746863ffef972aa999
Andrew Cao <andrewcao618@gmail.com>
2026-03-25 01:01:02 -0400

    refactored main files

--- backend/src/main/java/com/hatchloom/connecthub/connecthub_service/controller/MessageController.java
+++ backend/src/main/java/com/hatchloom/connecthub/connecthub_service/controller/MessageController.java
@@ -4,6 +4,7 @@ import com.hatchloom.connecthub.connecthub_service.dto.MessageResponse;
 import com.hatchloom.connecthub.connecthub_service.dto.SendMessageRequest;
 import com.hatchloom.connecthub.connecthub_service.dto.SendMessageResponse;
 import com.hatchloom.connecthub.connecthub_service.service.MessageService;
+import com.hatchloom.connecthub.connecthub_service.utils.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +19,18 @@ import java.util.UUID;
 @RequestMapping("/api/message")
 public class MessageController {
     private final MessageService messageService;
+    private final JwtUtil jwtUtil;
 
-    public MessageController(MessageService messageService) {
+    public MessageController(MessageService messageService, JwtUtil jwtUtil) {
         this.messageService = messageService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/{recipientId}/send")
-    public ResponseEntity<SendMessageResponse> sendMessage(@PathVariable UUID recipientId, @RequestBody SendMessageRequest request) {
+    public ResponseEntity<SendMessageResponse> sendMessage(@RequestHeader("Authorization") String authHeader, @PathVariable UUID recipientId, @RequestBody SendMessageRequest request) {
         try {
-            SendMessageResponse response = messageService.sendMessage(request.conversationId(), request.senderId(), recipientId, request.content());
+            UUID senderId = jwtUtil.extractUserId(authHeader);
+            SendMessageResponse response = messageService.sendMessage(request.conversationId(), senderId, recipientId, request.content());
             return new ResponseEntity<>(response, HttpStatus.CREATED);
         }
         catch (IllegalArgumentException e) {
@@ -35,8 +39,9 @@ public class MessageController {
     }
 
     @GetMapping("/conversation/{conversationId}")
-    public ResponseEntity<List<MessageResponse>> getConversationMessages(@PathVariable UUID conversationId, @RequestParam UUID userId) {
+    public ResponseEntity<List<MessageResponse>> getConversationMessages(@PathVariable UUID conversationId, @RequestHeader("Authorization") String authHeader) {
         try {
+            UUID userId = jwtUtil.extractUserId(authHeader);
             List<MessageResponse> msgs = messageService.getConversationMessages(conversationId, userId);
             return new ResponseEntity<>(msgs, HttpStatus.OK);
         }
